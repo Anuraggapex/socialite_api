44f38f40b93fef02bc4bae5fef5bb25ff672e557
Anurag_312006 <anuragghosh.312006@gmail.com>
2026-08-15 00:25:28 +0530

    Refactor user router for consistency and clarity

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,32 +1,35 @@
-from fastapi import status,HTTPException,Depends,APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from .. import models,schemas,utils
+
+from .. import models, schemas, utils
 from ..database import get_db
 
-router=APIRouter(
-    prefix="/users",
-    tags=['Users']
+router = APIRouter(prefix="/users", tags=["Users"])
+
+
+@router.post(
+    "/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut
 )
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
 
-@router.post("",status_code=status.HTTP_201_CREATED,response_model=schemas.user_out)
-def create_user(user:schemas.user_create,db:Session=Depends(get_db)):
-    hashed_password=utils.hash(user.password)
-    user.password=hashed_password
-
-    new_user=models.user(**user.dict())
+    new_user = models.User(**user.model_dump())
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
+
     return new_user
 
-@router.get("/{id}",response_model=schemas.user_out)
-def get_user(id:int,db:Session=Depends(get_db)):
-    user=db.query(models.user).filter(models.user.id==id).first()
+
+@router.get("/{id}", response_model=schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
 
     if not user:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User with id: {id} does not exist"
+            detail=f"User with id: {id} does not exist",
         )
 
     return user
