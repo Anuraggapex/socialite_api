58053774061afe30b428fd356fb61a91cbbb372d
Anurag_312006 <anuragghosh.312006@gmail.com>
2026-08-15 00:05:01 +0530

    Refactor Alembic env.py for improved readability

--- alembic/env.py
+++ alembic/env.py
@@ -3,7 +3,6 @@ from logging.config import fileConfig
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 
-# Import Base containing models
 from app.models import Base
 
 config = context.config
@@ -11,12 +10,11 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# Link Alembic target metadata to SQLAlchemy models
-target_metadata = Base.metadata
+target_metadata=Base.metadata
 
 
 def run_migrations_offline() -> None:
-    url = config.get_main_option("sqlalchemy.url")
+    url=config.get_main_option("sqlalchemy.url")
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -29,7 +27,7 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    connectable = engine_from_config(
+    connectable=engine_from_config(
         config.get_section(config.config_ini_section, {}),
         prefix="sqlalchemy.",
         poolclass=pool.NullPool,
