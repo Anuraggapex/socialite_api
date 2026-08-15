4745b5ace2818758c5d24b8aabb1c44f279ed8b1
Anurag_312006 <anuragghosh.312006@gmail.com>
2026-08-15 09:37:16 +0530

    database connection string construction
    Updated database connection string to use settings from config.

--- app/database.py
+++ app/database.py
@@ -1,15 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
 
-SQLALCHEMY_DATABASE_URL='postgresql://postgres:Pavu%402631@localhost/fastapi'
-
+# Dynamically construct the DB connection string from pydantic 
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 engine=create_engine(SQLALCHEMY_DATABASE_URL)
-
-SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
-
+SessionLocal=sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base=declarative_base()
 
+# Dependency to get DB session per request
 def get_db():
     db=SessionLocal()
     try:
